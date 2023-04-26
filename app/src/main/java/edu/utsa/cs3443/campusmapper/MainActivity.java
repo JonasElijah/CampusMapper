fb7d4dfc6ef41b75cc1a03f9078223ce6f0de792
Jonas Elijah Icban <jonaselijah11@gmail.com>
2023-04-26 13:40:05 -0500

    First push

--- app/src/main/java/edu/utsa/cs3443/campusmapper/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/campusmapper/MainActivity.java
@@ -2,13 +2,27 @@ package edu.utsa.cs3443.campusmapper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import edu.utsa.cs3443.campusmapper.controller.Button1_MainController;
 
+public class MainActivity extends AppCompatActivity
+{
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText t1 = findViewById(R.id.editTextText);
+        EditText t2 = findViewById(R.id.editTextText2);
+
+        (findViewById(R.id.button1)).setOnClickListener(new Button1_MainController(this,t1,t2));
     }
 }
---
+++ app/src/main/java/edu/utsa/cs3443/campusmapper/controller/Button1_MainController.java
@@ -0,0 +1,33 @@
+package edu.utsa.cs3443.campusmapper.controller;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import edu.utsa.cs3443.campusmapper.CoursesActivity;
+
+public class Button1_MainController implements View.OnClickListener
+{
+    Context context;
+    EditText name, abc123;
+    public Button1_MainController(Context x, EditText y, EditText z)
+    {
+        context = x;
+        name = y;
+        abc123 = z;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        String name_string = name.getText().toString();
+        String abc123_string = abc123.getText().toString();
+
+        Intent intent = new Intent(context, CoursesActivity.class);
+        intent.putExtra("name",name_string);
+        intent.putExtra("abc123",abc123_string);
+        context.startActivity(intent);
+    }
+}
